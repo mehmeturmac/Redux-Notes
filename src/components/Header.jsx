7a8570edf90bc7b29bb837862e0c7d86b331281b
mehmeturmac <mehmeturmac@gmail.com>
2022-09-09 21:37:20 +0300

    Dark mode added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { Input, Text, Flex, Box, Button, useColorMode } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Icon, Input, Text, Flex, Box, Button, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useDispatch } from 'react-redux';
 import { search } from '../redux/notes/notesSlice';
 
 function Header() {
   const dispatch = useDispatch();
-
   const { colorMode, toggleColorMode } = useColorMode();
+  const [user, setUser] = useState(localStorage.getItem('user') || 'Log In');
+
+  const handleUser = () => {
+    const username = window.prompt('Enter your username?');
+    if (username.length > 0) {
+      setUser(username);
+      localStorage.setItem('user', username);
+    } else {
+      localStorage.removeItem('user');
+    }
+  };
 
   return (
     <header>
-      <Flex justifyContent="center" mt={10}>
-        <Box w={['80%', '60%', '40%', '30%']}>
+      <Flex justifyContent="center" mt={2}>
+        <Box w={['80%', '60%', '40%', '30%']} display="flex" flexDirection="column">
+          <Box alignSelf="end">
+            <Button bg="transparent" border={'1px solid'} mr={2} onClick={handleUser}>
+              {user}
+            </Button>
+            <Button bg="transparent" onClick={toggleColorMode}>
+              {colorMode === 'light' ? <Icon as={MoonIcon} bg="none" /> : <Icon as={SunIcon} bg="none" />}
+            </Button>
+          </Box>
           <Text textAlign="center" color="gray" fontSize={25} fontWeight="semibold">
             Notes App
           </Text>
-          <Button onClick={toggleColorMode}>Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</Button>
           <Input placeholder="Search..." mt={5} borderRadius={20} bg="white" boxShadow="lg" onChange={(e) => dispatch(search(e.target.value))} />
         </Box>
       </Flex>
--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>
 );
